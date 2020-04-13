025dc919f88bde82e7067a2ced76ee779ae08633
Carlos Delfino - Sony 1 <consultoria@carlosdelfino.eti.br>
2020-04-13 02:32:47 -0300

    Adicionado scripts MaxiGos e inciado tradução português

---
+++ assets/js/maxigos/i18n/mgos-i18n-pt-br.js
@@ -0,0 +1,17 @@
+// begin of localization
+if (typeof mxG=='undefined') mxG={};
+if (typeof mxG.Z=='undefined') mxG.Z=[];
+if (!mxG.Z.pt_br) mxG.Z['pt-br']=[];
+
+mxG.Z['pt-br']["•"]=1; // particle style
+
+// mgos.js
+mxG.Z['pt-br']["Require HTML5!"]="Requer HTML5!";
+mxG.Z['pt-br']["Loading..."]="Carregando...";
+
+// mgosSolve.js
+mxG.Z['pt-br']["Retry"]="Repetir";
+mxG.Z['pt-br']["Undo"]="Desfazer";
+mxG.Z['pt-br']["Continuation"]="Continuação";
+
+// end of localization
